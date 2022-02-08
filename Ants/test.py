2a589c4a85f27468cd21048ceaedc00b95ebfd12
Simenga2016 <trazanov.nc@gmail.com>
2022-02-08 21:58:18 +0300

    night
    some changes, correcting, but nothing serius

--- Ants/test.py
+++ Ants/test.py
@@ -5,7 +5,7 @@ import math
 RUN = 1
 WIDTH = 1280
 HEIGHT = 690
-FPS = 60
+FPS = 15
 ticks = 0
 secunds = 0
 
@@ -75,7 +75,7 @@ class Ants:
         self.pos[0]= self.pos[0]+x
         self.pos[1]=self.pos[1]+y
     def randnavchange(self):
-        self.nav += random.randint(-3000,3000)/500000
+        self.nav += random.randint(-3000,3000)/250000
         if self.nav>1:
             self.nav = 1
         if self.nav <-1:
@@ -90,10 +90,13 @@ class Ants:
             self.ymod = 1
         else:
             self.ymod = -1
-        distantion = dist(self.pos,objpos)
-        vector1 = [(-self.pos[0]+objpos[0])/distantion,(-self.pos[1]+objpos[1])/distantion]
-        vector2 = [1,0]
-        self.nav = findcos(vector1,vector2)
+        try:
+            distantion = dist(self.pos,objpos)
+            vector1 = [(-self.pos[0]+objpos[0])/distantion,(-self.pos[1]+objpos[1])/distantion]
+            vector2 = [1,0]
+            self.nav = findcos(vector1,vector2)
+        except:
+            print("TakeNav error!")
     def movetonav(self):
         self.pos[0]+=self.nav*self.speed
         self.pos[1]+=(1-(self.nav)**2)**0.5 * self.speed * self.ymod
@@ -134,6 +137,15 @@ class Ants:
 def CreateAnt(colony,pos,nav = 0.7,ymod = 1,speed = 1):
     a = Ants(pos,nav,ymod,speed)
     colony.append(a)
+def Scream(colony,screamer,state):
+    for i in colony:
+        if dist(i.pos,screamer.pos)<screamrange and screamer.steps[state]+screamrange < i.steps[state] and screamer != i :
+                    i.steps[state] = screamer.steps[state]+screamrange
+                    Scream(ants, i, state)
+                    if i.aim == state:
+                        i.takenav(screamer.pos)
+                        if showline == 1:
+                            pygame.draw.line(screen,(100*state,10,250-100*state),screamer.pos,i.pos)
 
 
 #Var's
@@ -141,44 +153,23 @@ if RUN:
     showline = 1
     fmb = 0
     screams = []
-    screamsuperrange = 80
-    screamersnum = 30
-    for i in range(0,screamersnum):
-        CreateAnt(screams, [random.randint(50, WIDTH - 50), random.randint(50, HEIGHT - 50)],
-                  round(random.randint(-50, 50) / 50 * 1000) / 1000 * random.choice([-1, 1]), random.choice([1, -1]),
-                  random.randint(20, 100) / 100)
-        screams[i].rad = 3
-        screams[i].speed*=2
-        screams[i].color = (50,100,255)
     l = []
     ints = []
     home = Interespoint([WIDTH/2,HEIGHT/2],0)
     home.pos = [random.randint(50,WIDTH-50),random.randint(50,HEIGHT-50)]
     interesnum = 2
-    antnum = 350
+    antnum = 500
     #screamrange = 1/(80*antnum/(WIDTH*HEIGHT))
-    screamrange = 40
+    screamrange = 38
     print(screamrange)
     ants = []
     for i in range(0,antnum):
-        CreateAnt(ants,[random.randint(50,WIDTH-50),random.randint(50,HEIGHT-50)],round(random.randint(-50,50)/50*1000)/1000*random.choice([-1,1]),random.choice([1,-1]),random.randint(40,1000)/1000)
+        CreateAnt(ants,[random.randint(50,WIDTH-50),random.randint(50,HEIGHT-50)],round(random.randint(-50,50)/50*1000)/1000*random.choice([-1,1]),random.choice([1,-1]),random.randint(500,1000)/1000)
     for i in range(0,interesnum):
         interes = Interespoint([random.randint(50,WIDTH-50),random.randint(50,HEIGHT-50)])
         #interes.pos = [WIDTH/5*3,HEIGHT/2]
         ints.append(interes)
 
-
-def Scream(colony,screamer,state,screamrange = screamrange):
-    for i in colony:
-        if screamer != i and dist(i.pos,screamer.pos)<screamrange and screamer.steps[state]+screamrange < i.steps[state]:
-                    i.steps[state] = screamer.steps[state]+screamrange
-                    Scream(ants, i, state)
-                    if state == 0:
-                        Scream(screams,i,0)
-                    if i.aim == state:
-                        i.takenav(screamer.pos)
-                        if showline == 1:
-                            pygame.draw.line(screen,(100*state,10,250-100*state),screamer.pos,i.pos)
 
 #Game cicle
 while RUN:
@@ -215,6 +206,10 @@ while RUN:
                 print(counter)
                 fmb = list(event.pos)
                 fmbrad = 10
+            if event.button == 4:
+                screamrange+=1
+            if event.button == 5:
+                screamrange-=1
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_d:
                     print("d")
@@ -238,34 +233,14 @@ while RUN:
     for i in ints:
         pygame.draw.circle(screen, (0, 200, 200), i.pos, 10)
     pygame.draw.circle(screen,(50,255,50),home.pos,15)
-    for i in screams:
-        Scream(ants,i,random.choice([0,1]),screamsuperrange)
-        i.movetonav()
-        for j in ints:
-            if dist(i.pos,j.pos)<10:
-                i.steps[1] = 0
-                i.nav=-i.nav
-                i.ymod*=-1
-        if dist(i.pos,home.pos)<15:
-            i.steps[0] = 0
-            i.aim = 1
-            i.nav = -i.nav
-            i.ymod *= -1
-        if i.pos[0]< 50 or i.pos[0]>WIDTH-50:
-            i.nav = -i.nav
-        if i.pos[1]<50 or i.pos[1]>HEIGHT-50:
-            i.ymod = -i.ymod
-        i.steps[0]+=1
-        i.steps[1]+=1
-        for k in range(0,10):
-            i.randnavchange()
-        i.drow()
     for i in ants:
-        #Scream(ants, i, random.choice([0,1]))
+        if random.randint(0,antnum) == 6:
+            Scream(ants, i, random.choice([0,1]))
         i.movetonav()
         i.steps[0]+=1
         i.steps[1]+=1
-        i.randnavchange()
+        for k in range(0,4):
+            i.randnavchange()
 
         if i.pos[0]< 50 or i.pos[0]>WIDTH-50:
             i.nav = -i.nav
@@ -274,8 +249,7 @@ while RUN:
         for j in ints:
             if dist(i.pos,j.pos)<10:
                 i.steps[1] = 0
-                if random.randint(0, 5) == 3:
-                    Scream(ants, i, 1)
+                Scream(ants, i, 1)
                 if  i.aim == 1:
                     i.aim = 0
                     i.color = (255,0,0)
@@ -283,8 +257,7 @@ while RUN:
                     i.ymod*=-1
         if dist(i.pos,home.pos)<15:
             i.steps[0] = 0
-            if random.randint(0,10) == 6:
-                Scream(ants, i, 0)
+            Scream(ants, i, 0)
             if  i.aim == 0:
                 i.aim = 1
                 i.color = (0,0,255)
